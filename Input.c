e4a35eed74f91b6296820facf29dfd5fb96b5fd2
shikhasingh4816-web <shikhasingh4816@gmail.com>
2026-06-25 12:37:34 +0530

    Add files via upload

---
+++ Input.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+
+int main()  {
+    int a,b;
+
+    printf("enter a \n");
+    scanf("%d", &a);
+
+    printf("enter b \n");
+    scanf("%d", &b);
+
+    printf("Sum is : %d \n",a+b);
+    return 0;
+
+
+}
