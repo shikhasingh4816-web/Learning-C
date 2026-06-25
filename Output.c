a168886b7b0bf871a44ea495cb2f523d8e23f9b4
shikhasingh4816-web <shikhasingh4816@gmail.com>
2026-06-25 12:29:47 +0530

    Add files via upload

---
+++ Output.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+
+int main()  {
+    int age = 20;
+    float pi = 3.14;
+    char percentage = '%';
+
+    printf("age is %d", age);
+    printf("age is %f", pi);
+    printf("age is %c", percentage);
+    return 0;
+
+}
+
